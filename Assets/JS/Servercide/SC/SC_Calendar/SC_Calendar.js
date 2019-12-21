647ef15c2734f22eda1aeac8c22c528d4fd31d3d
Devin-M-D <downer.devin@gmail.com>
2019-12-21 10:32:45 -0600

    budget update 4
    - confirmed I can use jquery cal for this
    - added classes and text to date
    - need to figure out logic/math

--- Assets/JS/Servercide/SC/SC_Calendar/SC_Calendar.js
+++ Assets/JS/Servercide/SC/SC_Calendar/SC_Calendar.js
@@ -2,7 +2,24 @@
 //V1.0
 ////////////////////////////
 var SC_Calendar = function (element) {
-  var defaultParams = {};
+  var defaultParams = {
+    src: "",
+    data: {
+      accounts: [
+        {
+          name: "Bank1",
+          balance: 100,
+          asOfDate: "2019-12-11"
+        }
+      ],
+      recurringExpenses: [
+        {
+          amount: 10,
+          recurrencePattern: "every 1st of month"
+        }
+      ]
+    }
+  };
   var promise = ServercideApp.call(
     this, element, "SC_Calendar", defaultParams,
     SC_Calendar.prototype.onStrap
@@ -14,20 +31,36 @@ SC_Calendar.prototype = Object.create(ServercideApp.prototype);
 SC_Calendar.prototype.onStrap = function (app) {
   app.debugMsg(app.element.attr("id") + " app " + app.getMetaParam("type") + " is strapping, running onStrap before recursion.", 2);
   return new Promise(function (fulfill, reject) {
+    var activeMonth = new Date().getMonth() + 1;
+    var activeYear = new Date().getFullYear();
+
     app.element.datepicker({
       showOtherMonths: true,
-      selectOtherMonths: true
+      selectOtherMonths: true,
+      onChangeMonthYear: function (year, month, inst) {
+        // activeMonth = month;
+        // activeYear = year;
+        // setTimeout(() => { app.element.find('.ui-datepicker-month').css("height", "unset") }, 0);
+        // setTimeout(() => { app.element.find('.ui-datepicker-year').css("height", "unset") }, 0);
+        // app.calcCurrMonthBoMBalance(app);
+      },
+      beforeShowDay: function (date) {
+        console.log(date)
+        var activeDay = date.getDay();
+        return [true, `date${activeYear}-${activeMonth}-${activeDay}`, ""];
+      }
     });
-    // app.element.find('.ui-datepicker-month').each(function(month){
-    //   month.css("height", "unset");
-    // });
-    // app.element.find('.ui-datepicker-year').each(function(year){
-    //   year.css("height", "unset");
-    // });
-    //console.log(app.element.find('.ui-datepicker-month'));
+
     app.element.find('.ui-datepicker-month').css("height", "unset");
     app.element.find('.ui-datepicker-year').css("height", "unset");
+
+    // app.element.find("[data-month='11'] [data-year='2019']")
     fulfill();
   });
+}
+
+SC_Calendar.prototype.calcCurrMonthBoMBalance = function (app) {
+  // var accounts = app.getParam(["data", "accounts"]);
+  // console.log(accounts);
 }
 ////////////////////////////
